40a95058c2b565a544cbb20a0d298b326a1a2107
25081997pc <chavanpooja2508@gmail.com>
2022-06-17 20:05:20 +0530

    TC1.2

--- MoodAnalyser.Test/MoodAnalyserTest1.cs
+++ MoodAnalyser.Test/MoodAnalyserTest1.cs
@@ -20,5 +20,22 @@ namespace MoodAnalyser.Test
             Assert.AreEqual(expexted_Output, actual_Output);
 
         }
+        [TestMethod]
+        public void TestanalyseMoodHappy()
+        {
+            MoodAnalyse moodAnalyse = new MoodAnalyse();
+
+            //Arrange
+            string messege = "I am in any Mood";
+            string expexted_Output = "Happy Mood";
+            string actual_Output;
+
+            //Act
+            actual_Output = moodAnalyse.AnalyseMood(messege.ToLower());
+
+            //Assert
+            Assert.AreEqual(expexted_Output, actual_Output);
+
+        }
     }
 }
